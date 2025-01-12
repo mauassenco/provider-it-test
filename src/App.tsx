d652b87e85261f684f480b0ab632c6c3c970a5f3
mauassenco <mau.assenco@gmail.com>
2025-01-12 13:30:20 -0300

    chore: add  new dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,47 @@
-import './App.css';
+import { ThemeProvider } from 'styled-components';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import GlobalStyles from './Globals';
+
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import NotFound from './pages/NotFound';
+
+import Header from './components/Layout/Header';
+
+const theme = {
+  colors: {
+    primary: '#fefefe',
+    secondary: '#ebfbff',
+    dark: '#003333',
+  },
+  mobile: '768px',
+};
 
 function App() {
   return (
-    <>
-      <div className="">Home</div>
-    </>
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <Router>
+          <div>
+            <Header />
+            <main style={{ zIndex: '1', position: 'relative' }}>
+              <Routes>
+                <Route path="/" element={<Signin />} />
+                <Route path="/signin" element={<Signin />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/notfound" element={<NotFound />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </main>
+          </div>
+        </Router>
+
+        {/* <SignInForm /> */}
+        {/* <SignUpForm /> */}
+      </>
+    </ThemeProvider>
   );
 }
 

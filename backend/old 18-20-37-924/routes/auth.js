4f876898cb55c383bf4b4185c35efe832af6aa1a
mauassenco <mau.assenco@gmail.com>
2025-01-14 19:14:12 -0300

    fix: removendo arquivos antigos

--- backend/old 18-20-37-924/routes/auth.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-router.post('/users', authController.handleLogin);
-
-module.exports = router;
